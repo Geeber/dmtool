e44c4ae72c855c86f79c1fe9d9c29f712a44f904
Kevin Litwack <kevin.litwack@gmail.com>
2015-02-28 14:23:06 -0800

    Add dev profile

---
+++ dev/user.clj
@@ -0,0 +1,8 @@
+(ns user
+  (:require
+    [clojure.repl :refer :all]
+    [clojure.tools.namespace.repl :refer [refresh]]
+    [dmtool.dice :as dice]
+    [dmtool.stats :as stats]
+    )
+  )
--- project.clj
+++ project.clj
@@ -11,4 +11,7 @@
                  [ring/ring "1.3.2"]
                  [ring/ring-json "0.3.1"]
                  [hiccup "1.0.5"]
-                 [compojure "1.3.2"]])
+                 [compojure "1.3.2"]]
+  :profiles {:repl {:source-paths ["dev"]
+                    :dependencies [[org.clojure/tools.namespace "0.2.8"]]}}
+  )
