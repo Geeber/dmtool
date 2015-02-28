87ec4a816034597487fa8b7e53b0cfaeadac45ba
Kevin Litwack <kevin.litwack@gmail.com>
2015-02-28 15:23:06 -0800

    Add normal 4d6d1 stats method

--- src/dmtool/stats.clj
+++ src/dmtool/stats.clj
@@ -4,3 +4,7 @@
 (defn hardcore-stats
   []
     (reverse (sort (take 6 (repeatedly #(dice/roll 3 6))))))
+
+(defn normal-stats
+  []
+    (reverse (sort (take 6 (repeatedly #(dice/roll 4 6 (dice/drop-low 1)))))))
--- test/dmtool/stats_test.clj
+++ test/dmtool/stats_test.clj
@@ -3,6 +3,16 @@
             [dmtool.stats :refer :all]
             [dmtool.dice :as dice]))
 
+(deftest test-normal-stats
+  (testing "Generate stats using the normal (4d6 drop 1) method."
+    (dice/with-rolls {6 [1 2 3 5
+                         6 3 3 4
+                         1 3 4 2
+                         4 6 3 4
+                         2 1 1 4
+                         6 2 5 6]}
+      (is (= (normal-stats) [17 14 13 10 9 7])))))
+
 (deftest test-hardcore-stats
   (testing "Generate stats using the hardcore method."
     (dice/with-rolls {6 [2 2 2 4 4 4 1 1 1 5 5 5 6 6 6 3 3 3]}
